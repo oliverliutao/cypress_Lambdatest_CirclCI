76ea5fb3b399e90f157c6079f7186f33f8dda727
TAOLIU <liutaooliver@gmail.com>
2021-06-13 23:42:13 +0800

    try cypress API

--- cypress/integration/examples/gihealthcheck.spec.js
+++ cypress/integration/examples/gihealthcheck.spec.js
@@ -1,6 +1,16 @@
 describe('my first test', function(){
     
     it.only('Visits the GI Home', () => {
+
+        // cy.viewport('ipad-mini')
+        cy.log('Cypress.version : ' + Cypress.version)
+        cy.log('platform : ' + Cypress.platform)
+        cy.log('CPU architecture : ' + Cypress.arch)
+        cy.log('testing browser : ' + Cypress.browser.name)
+        cy.log('specs under testing : ' + Cypress.spec.name)
+        cy.log('Cypress.testingType : ' + Cypress.testingType)
+
+
         cy.visit('/hm')
         
         cy.contains('Landlord').click()
@@ -10,13 +20,22 @@ describe('my first test', function(){
         cy.contains('Choose plan').click()
 
         cy.url().should('include', '/addOns')
+        
+        // first "Select" btn
+        // cy.get('tbody a').first().click()
 
-        cy.wait(10000)
+        // last "Select" btn
+        cy.get('tbody a').last().click().pause()
+        
+        // cy.log('this is log')
 
+        //second "Select" btn
+        // cy.get('tbody td:first').next().get('a').click()
+
+        cy.url().should('include', '/addRider')
 
         
 
-        // cy.url().should('include', '/addRider')
 
         // cy.contains('Go to personal details').click()
 
